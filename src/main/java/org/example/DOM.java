07fb3dda524bc5cb819f43b1284df32299b16d1f
CHNI4 <nicolas.chapatte@coop.ch>
2023-11-07 16:20:44 +0100

    Car and css

--- src/main/java/org/example/DOM.java
+++ src/main/java/org/example/DOM.java
@@ -3,6 +3,8 @@ package org.example;
 public class DOM {
 
     public static void main(String[] args){
+        scene.getStylesheets().add(getClass().getResource("DOMstyle.css").toExternalForm());
 
     }
+
 }
